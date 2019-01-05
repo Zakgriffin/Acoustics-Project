95d24a99d6a932391b65621b06a1043489189ada
dtowle17 <dtowle17@gmail.com>
2019-01-05 00:03:05 -0500

    THIS IS EXPERIMENTAL FIX TO CROSSED WALLS

---
+++ Arc.java
@@ -0,0 +1,578 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+import processing.core.PApplet;
+
+/*
+ * Current Bugs:
+ *		* Understood and being fixed
+ *		• Still looking into source of issue
+ *
+ * 		generateArcs()
+ *			• having right angles for min and max angle limits causes issues
+ *			* closed shapes formed by walls sometimes seem to have small
+ *				holes in connections or create large arcs that ignore some walls
+ */
+
+public class Arc {
+	
+	// Arcs are used to visualize pressure waves
+	
+	public static ArrayList<Arc> arcs = new ArrayList<Arc>();
+	
+	private static PApplet p;
+	private Point pos;
+	private String type;
+	
+	private float minAngLim;
+	private float maxAngLim;
+	
+	float perpDist;
+	
+	private Wall wall;
+	private Wall rebound;
+	
+	public Arc(float minAngLim, float maxAngLim, Wall wall, Point pos, String type) {
+		this.minAngLim = Geo.simpAngle(minAngLim);
+		this.maxAngLim = Geo.simpAngle(maxAngLim);
+		this.wall = wall;
+		this.pos = pos;
+		this.type = type;
+		//arcs.add(this);
+	}
+	public Arc(float minAngLim, float maxAngLim, Wall wall, Point pos) {
+		this(minAngLim, maxAngLim, wall, pos, "");
+	}
+	
+	public Arc(float minAngLim, float maxAngLim, Point pos) {
+		this(minAngLim, maxAngLim, null, pos, "passing");
+	}
+	
+	public static void setP(PApplet pa) {
+		p = pa;
+	}
+	
+	public void show(float time) {
+		/*
+		//DEBUG
+		float g = 0;
+		if(wall != null) {
+			g = wall.color[0] / 10f;
+		}
+		p.ellipse(pos.x, pos.y, 5 + g, 5 + g);
+		*/
+		
+		float outerMin = minAngLim;
+		float innerMin = -1; // <-- -1 used as flag
+		float innerMax = -1;
+		float outerMax = maxAngLim;
+		
+		// stuff for rebound walls
+		if(rebound != null) {
+			// arc is not from source
+			Point[] rebInters = Geo.segCircIntersects(rebound.getPoints(), pos, time);
+			if(rebInters == null) {
+				// arc has not traveled far enough to display
+				return;
+			}
+			AngVect[] rebOrd = orderMinMax(rebInters, pos);
+			float minAng = rebOrd[0].angle;
+			float maxAng = rebOrd[1].angle;
+			if(!minLimFallsIn(minAng, maxAng) && !maxLimFallsIn(minAng, maxAng)) {
+				// arc still has not traveled far enough to display
+				return;
+			}
+			if(angleInMinMax(minAng)) {
+				outerMin = minAng;
+			}
+			if(angleInMinMax(maxAng)) {
+				outerMax = maxAng;
+			}
+		}
+		
+		// stuff for walls that arcs crash into
+		if(!type.equals("passing")) {
+			Point[] wallInters =  Geo.segCircIntersects(wall.getPoints(), pos, time);
+			if(wallInters != null) {
+				AngVect[] wallOrd = orderMinMax(wallInters, pos);
+				float minAng = wallOrd[0].angle;
+				float maxAng = wallOrd[1].angle;
+				if(minLimFallsIn(minAng, maxAng) && maxLimFallsIn(minAng, maxAng)) {
+					// arc has traveled too far to display
+					return;
+				}
+				if(angleInMinMax(minAng)) {
+					innerMin = minAng;
+				}
+				if(angleInMinMax(maxAng)) {
+					innerMax = maxAng;
+				}
+			}
+		}
+		
+		p.pushStyle();
+			p.noFill();
+			if(rebound != null) {
+				int[] r = rebound.color;
+				p.stroke(r[0], r[1], r[2]);
+			} else {
+				p.stroke(0);
+			}
+			arcDisplay(time - 1f, outerMin, innerMin, innerMax, outerMax);
+			
+			// assign colors and draw final arcs
+			if(!type.equals("passing")) {
+				int[] w = wall.color;
+				p.stroke(w[0], w[1], w[2]);
+			} else {
+				p.stroke(255);
+			}
+			
+			arcDisplay(time, outerMin, innerMin, innerMax, outerMax);
+		p.popStyle();
+	}
+	private void arcDisplay(float time, float outerMin, float innerMin, float innerMax, float outerMax) {
+		if(innerMin == -1 && innerMax == -1) {
+			// single arc
+			arc(pos.x, pos.y, time * 2, outerMin, outerMax);
+		} else {
+			// partial arcs
+			if(innerMin != -1) {
+				arc(pos.x, pos.y, time * 2, outerMin, innerMin);
+			}
+			if(innerMax != -1) {
+				arc(pos.x, pos.y, time * 2, innerMax, outerMax);
+			}
+		}
+	}
+	
+	public boolean hasCrossed(float time) {
+		// Returns whether or not this arc has crossed its wall
+		return hasDone(time, "cross");
+	}
+	
+	public boolean hasTouched(float time) {
+		// Returns whether or not this arc has touched its wall
+		return hasDone(time, "touch");
+	}
+	
+	private boolean hasDone(float time, String action) {
+		if(!type.equals("passing")) {
+			Point[] wallInters =  Geo.segCircIntersects(wall.getPoints(), pos, time);
+			if(wallInters != null) {
+				AngVect[] wallOrd = orderMinMax(wallInters, pos);
+				float minAng = wallOrd[0].angle;
+				float maxAng = wallOrd[1].angle;
+				if(action.equals("cross")) {
+					// extension of  hasCrossed() function
+					if(minLimFallsIn(minAng, maxAng) && maxLimFallsIn(minAng, maxAng)) {
+						return true;
+					}
+				} else if(action.equals("touch")){
+					// extension of hasTouched() function
+					if(angleInMinMax(minAng) || angleInMinMax(maxAng) || minLimFallsIn(minAng, maxAng)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	private static int blend(int start, int with, int weight) {
+		int result = start * weight;
+		result += with;
+		result /= weight + 1;
+		
+		return result;
+	}
+	
+	private boolean angleInMinMax(float angle) {
+		return Geo.angleFallsIn(angle, minAngLim, maxAngLim);
+	}
+	private boolean minLimFallsIn(float minAng, float maxAng) {
+		return Geo.angleFallsIn(minAngLim, minAng, maxAng);
+	}
+	private boolean maxLimFallsIn(float minAng, float maxAng) {
+		return Geo.angleFallsIn(maxAngLim, minAng, maxAng);
+	}
+	/*
+	public void showWithFill(float time) {
+		// Mostly used for debugging
+		p.pushStyle();
+		if(!type.equals("passing")) {
+			// reflective arc
+			
+			int[] a = wall.color;
+			//p.fill(a[0], a[1], a[2], 60);
+			p.noFill();
+			p.stroke(a[0], a[1], a[2]);
+			
+			// get the intersection points between walls and arcs
+			Point[] inters = Geo.segCircIntersects(wall.getPoint1(), wall.getPoint2(), pos, time);
+			
+			boolean simpleArc = true;
+			if(inters != null) {
+				// points on wall visible to pos
+				Point[] visPoints = new Point[2];
+				visPoints[0] = Geo.raySegIntersect(pos, minAngLim, wall.getPoints(), true);
+				visPoints[1] = Geo.raySegIntersect(pos, maxAngLim, wall.getPoints(), true);
+				if(Geo.rectIntersect(visPoints, inters)) {
+					simpleArc = false;
+				}
+			}
+			
+			if(simpleArc) {//simpleArc
+				// arc has no interaction with wall and can be rendered simply
+				arc(pos.x, pos.y, time * 2, minAngLim, maxAngLim);
+			} else {
+				// arc is split into 3 pieces: 2 small arcs, 1 triangle
+				AngVect[] limits = orderMinMax(inters, pos);
+				Point pMin = limits[0].pos;
+				Point pMax = limits[1].pos;
+				float minAng = limits[0].angle;
+				float maxAng = limits[1].angle;
+				
+				if(Geo.angleFallsIn(minAng, minAngLim, maxAngLim)) {
+					// ensure intersection point is touching wall
+					arc(pos.x, pos.y, time * 2, minAngLim, minAng);
+				} else {
+					// if not, don't draw arc part and make triangle use wall end point
+					pMin = Geo.raySegIntersect(pos, minAngLim, wall.getPoints(), true);
+				}
+				// repeat ^
+				if(Geo.angleFallsIn(maxAng, minAngLim, maxAngLim)) {
+					arc(pos.x, pos.y, time * 2, maxAng, maxAngLim);
+				} else {
+					pMax = Geo.raySegIntersect(pos, maxAngLim, wall.getPoints(), true);
+				}
+				
+				p.pushStyle();
+					p.noStroke();
+					p.triangle(pMin.x, pMin.y, pMax.x, pMax.y, pos.x, pos.y);
+				p.popStyle();
+			}
+		} else {
+			// passing arc
+			p.noFill();
+			p.stroke(255);
+			arc(pos.x, pos.y, time * 2, minAngLim, maxAngLim);
+		}
+		p.popStyle();
+	}
+	*/
+	
+	
+	private AngVect[] orderMinMax(Point[] unorg, Point view, Wall wall) {
+		Point pMin = unorg[0];
+		Point pMax = unorg[1];
+		float minAng = pos.angleTo(pMin);
+		float maxAng = pos.angleTo(pMax);
+		
+		if(Geo.firstIsMin(maxAng, minAng)) {
+			// flip points and angles if reversed
+			float tempA = minAng;
+			minAng = maxAng;
+			maxAng = tempA;
+			Point tempP = pMin;
+			pMin = pMax;
+			pMax = tempP;
+		}
+		
+		AngVect min = new AngVect(pMin, minAng, wall, true);
+		AngVect max = new AngVect(pMax, maxAng, wall, false);
+		
+		// set linked references to each other
+		min.link = max;
+		max.link = min;
+		return new AngVect[] {min, max};
+	}
+	private AngVect[] orderMinMax(Point[] unorg, Point view) {
+		return orderMinMax(unorg, view, null);
+	}
+	private AngVect[] orderMinMax(Wall wall, Point view) {
+		return orderMinMax(wall.getPoints(), view, wall);
+	}
+	
+	public ArrayList<Arc> generateArcs() {
+		ArrayList<AngVect> angs = new ArrayList<AngVect>();
+		ArrayList<AngVect> started = new ArrayList<AngVect>();
+		int wallOrient = 0;
+		
+		if(rebound != null) {
+			wallOrient = Geo.orientation(rebound.getPoints(), pos);
+		}
+		
+		for(Wall wallCheck: Wall.walls) {
+			// create and add all AngVects to angs and started lists
+			
+			AngVect[] wallPoints = orderMinMax(wallCheck, pos);
+			AngVect min = wallPoints[0];
+			AngVect max = wallPoints[1];
+			
+			if(rebound != null && isBehindRebound(wallPoints, wallOrient)) {
+				// wall is behind rebound and should be skipped
+				continue;
+			}
+			if(min.angle == max.angle) {
+				// wall is in line with pos and should be skipped
+				continue;
+			}
+			if(Geo.angleFallsIn(min.angle, minAngLim, maxAngLim)) {
+				angs.add(min);
+			}
+			
+			if(Geo.angleFallsIn(max.angle, minAngLim, maxAngLim)) {
+				angs.add(max);
+			}
+			
+			// wall crosses over minAngLim
+			if(Geo.angleFallsIn(minAngLim, min.angle, max.angle)) {
+				started.add(min);
+			}
+		}
+		ArrayList<Arc> subArcs = new ArrayList<Arc>();
+		Collections.sort(angs);
+		// rotate angs list to start after minAngLim and end before maxAngLim
+		Collections.rotate(angs, getRotatePoint(angs));
+		
+		AngVect current; // <-- currently selected min AngVect on sweep
+		float lastAngle; // <-- used to form max angle of last sub arc and min of the next
+
+		if(started.size() == 0) {
+			if(angs.size() == 0) {
+				// sub arcs is only this arc. End and return here
+				subArcs.add(this);
+				return subArcs;
+			} else {
+				// first subarc will be passing. From minAngLim to first in angs
+				subArcs.add(new Arc(minAngLim, angs.get(0).angle, pos));
+				current = angs.get(0);
+				lastAngle = current.angle;
+				//started.add(current); // add to started since it was skipped
+			}
+		} else {
+			// set current to AngVect closest to pos
+			current = findBest(started, minAngLim, true);
+			lastAngle = minAngLim;
+		}
+		
+		for(int i = 0; i < angs.size(); i++) {
+			// cycle through all in angs: sweep check
+			AngVect check = angs.get(i); // <-- current AngVect to check
+			AngVect next = null;
+			if(i < angs.size() - 1) {
+				next = angs.get(i + 1);
+			}
+			
+			if(check.isMin) {
+				// began new wall
+				started.add(check);
+			} else {
+				// hit end of some wall
+				started.remove(check.link);
+			}
+			/*
+			if(next != null && check.angle == next.angle) {
+				//System.out.println("HERE!!!!");
+				if(next.angle == check.angle) {
+					p.pushStyle();
+						p.stroke(255, 0, 0);
+						//p.line(pos.x, pos.y, pos.x + Main.time * (float) Math.cos(check.angle), pos.y + Main.time * (float) Math.sin(check.angle));
+					p.popStyle();
+				}
+				
+				// check if next angle is equal to checks
+				AngVect closest = null;
+				float dist = -1;
+				int count;
+				for(count = 0; i + count < angs.size() && angs.get(i + count).angle == check.angle; count++) {
+					// go through all AngVects in angs with same angle
+					AngVect newClosest = angs.get(i + count);
+					if(newClosest.isMin) {
+						// must be min to consider
+						float newDist = pos.distanceTo(newClosest.pos);
+						if(newDist < dist || dist == -1) {
+							closest = newClosest;
+							dist = newDist;
+						}
+					} else {
+						// max so wall ended, remove from started list
+						started.remove(newClosest.link);
+					}
+				}
+				
+				subArcs.add(new Arc(lastAngle, check.angle, current.wall, pos));
+				lastAngle = check.angle;
+				
+				if(closest != null) {
+					// found best candidate, similar to interupt
+					if(!current.wall.intersects(pos, closest.pos)) {
+						// make sure closest pos is behing current wall
+						current = closest;
+					}
+				} else {
+					// all walls with same angle were max, current becomes next AngVect after these
+					if(i + count < angs.size()) {
+						current = angs.get(i + count);
+					}
+				}
+				
+				// increment i to skip over all AngVects with this angle
+				i += count - 1;
+				// END OF repeated angle case and this iteration of the loop
+			} else */if(check == current.link) {
+				// got to end of current wall
+				
+				// add arc before fall
+				subArcs.add(new Arc(lastAngle, check.angle, check.wall, pos));
+				lastAngle = check.angle;
+				if(i == angs.size() - 1) {
+					break;
+				}
+				AngVect best = findBest(started, check.pos.slopeWith(pos));
+				if(best != null) {
+					// found wall to set current to
+					current = best;
+					// END OF drop wall type, continue;
+				} else {
+					// no wall to drop to, create passing arc in addition to the first
+					subArcs.add(new Arc(lastAngle, next.angle, pos));
+					
+					current = next;
+					lastAngle = next.angle;
+					// increase index again since two arcs were made
+					i++;
+					started.add(angs.get(i)); // add to started since it was skipped
+					// END OF drop wall with passing arc type, continue;
+				}
+			} else if(check.isMin) {
+				// check is a possible interrupt
+				if(!current.wall.intersects(pos, check.pos)) {
+					// check is interrupt
+					subArcs.add(new Arc(lastAngle, check.angle, current.wall, pos));
+					current = check;
+					lastAngle = check.angle;
+					// END OF interrupt wall type, continue;
+				}
+			}
+		}
+		// add final arc completing the sweep
+		AngVect best = findBest(started, maxAngLim, true);
+		if(best != null) {
+			// reflective arc
+			subArcs.add(new Arc(lastAngle, maxAngLim, best.wall, pos));
+		} else {
+			// passing arc
+			subArcs.add(new Arc(lastAngle, maxAngLim, pos));
+		}
+		
+		if(rebound != null) {
+			// used for drawing arcs to know where to start
+			for(Arc arc: subArcs) {
+				arc.setRebound(rebound);
+			}
+		}
+		return subArcs;
+	}
+	
+	public Arc mirrorArc() {
+		Point newPos = Geo.boost(pos, wall.getPerpPoint(pos));
+		float newMin = Geo.angleBoost(maxAngLim, wall.getAngle());
+		float newMax = Geo.angleBoost(minAngLim, wall.getAngle());
+		Arc mirror = new Arc(newMin, newMax, newPos);
+		mirror.setRebound(wall);
+		return mirror;
+	}
+	
+	private boolean isBehindRebound(AngVect[] tests, int wallOrient) {
+		// returns whether or not testWall is behind rebound and should not be considered
+		if(tests[0].wall == rebound) {
+			return true;
+		}
+		for(AngVect test: tests) {
+			if(wallOrient == Geo.orientation(rebound.getPoints(), test.pos)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private int getRotatePoint(ArrayList<AngVect> list) {
+		// Returns an index to rotate list by
+		
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).angle > minAngLim) {
+				return list.size() - i;
+			}
+		}
+		
+		return 0;
+	}
+	
+	private AngVect findBest(ArrayList<AngVect> started, float raySlope) {
+		AngVect best = null;
+		float bestDist = -1;
+		
+		// cycle through "started" containing all walls that have started
+		for(AngVect newBest: started) {
+			float newBestDist = Geo.raySegDist(pos, raySlope, newBest.pos, newBest.link.pos, true);
+			if(newBestDist < bestDist || bestDist == -1) {
+				best = newBest;
+				bestDist = newBestDist;
+			}
+		}
+		return best;
+		
+	}
+	private AngVect findBest(ArrayList<AngVect> started, float rayAngle, boolean isAng) {
+		if(isAng) {
+			return findBest(started, (float) Math.tan(rayAngle));
+		} else {
+			float raySlope = rayAngle;
+			return findBest(started, raySlope);
+		}
+		
+	}
+	
+	public static void arc(float x, float y, float r, float min, float max) {
+		// used instead of just calling p.arc since is max is less than min, nothing displays
+		if(max < min) {
+			max += Math.PI * 2;
+		}
+		p.arc(x, y, r, r, min, max);
+	}
+	
+	public Point getPos() {
+		return pos;
+	}
+	public void setPos(Point pos) {
+		this.pos = pos;
+	}
+	public void setPos(float x, float y) {
+		this.pos.x = x;
+		this.pos.y = y;
+	}
+	public float getMinLim() {
+		return minAngLim;
+	}
+	public float getMaxLim() {
+		return maxAngLim;
+	}
+	
+	public String toString() {
+		return "(" + minAngLim + " -> " + maxAngLim + ")";
+	}
+	
+	public Wall getWall() {
+		return wall;
+	}
+	public String getType() {
+		return type;
+	}
+	
+	private void setRebound(Wall rebound) {
+		this.rebound = rebound;
+	}
+}
---
+++ Geo.java
@@ -0,0 +1,324 @@
+public class Geo {
+	
+	// A class for holding static geometric type methods
+	
+	private static final float z = 0.001f;
+	private static final float PI = (float) Math.PI;
+	
+	private static final float FULL = PI * 2;
+	private static final float UP = PI + PI / 2;
+	private static final float DOWN = PI / 2;
+	// UP and DOWN are correct since Processing goes clockwise unlike traditional radians
+	
+	public static float slope(Point p1, Point p2) {
+		// Returns the slope between p1 and p2
+		return (p1.y - p2.y) / (p1.x - p2.x);
+	}
+	
+	public static float angle(Point p1, Point p2) {
+		// Returns the angle from p1 to p2
+		double angle = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+		if(angle < 0) angle += FULL;
+		return (float) angle;
+	}
+
+	public static float distance(Point p1, Point p2) {
+		// Returns the distance between p1 and p2
+		float xDif = p1.x - p2.x;
+		float yDif = p1.y - p2.y;
+		
+		return (float) Math.sqrt(xDif * xDif + yDif * yDif);
+	}
+
+	public static Point boost(Point p1, Point p2) {
+		// Returns the point double the distance from p1 as p2
+		return new Point(p2.x * 2 - p1.x, p2.y * 2 - p1.y);
+	}
+	
+	public static Point perpPoint(Point seg1, Point seg2, Point p) {
+		// Returns the closest point from p to a line segment (seg)
+		float px;
+		float py;
+		if(seg1.y == seg2.y) {
+			// horizontal line protection
+			px = p.x;
+			py = seg1.y;
+		} else if(seg1.x == seg2.x) {
+			// vertical line protection
+			px = seg1.x;
+			py = p.y;
+		} else {
+			float m1 = seg1.slopeWith(seg2);
+			float m2 = 1 / -m1;
+			px = (m2 * p.x - p.y + seg1.y - m1 * seg1.x) / (m2 - m1);
+			py = m1 * (px - seg1.x) + seg1.y;
+		}
+		return new Point(px, py);
+	}
+	
+	public static float raySegDist(Point ray1, float raySlope, Point seg1, Point seg2, boolean simp) {
+		// Returns distance between intersection point of a ray and line segment
+		Point inter = raySegIntersect(ray1, raySlope, seg1, seg2);
+		float deltaX = inter.x - ray1.x;
+		float deltaY = inter.y - ray1.y;
+		
+		float distSimp = deltaX * deltaX + deltaY * deltaY;
+		if(simp) {
+			// simplified distance for speed (use for comparing only)
+			return distSimp;
+		} else {
+			// actual distance
+			return (float) Math.sqrt(distSimp);
+		}
+	}
+	public static float raySegDist(Point ray1, float raySlope, Point seg1, Point seg2) {
+		return raySegDist(ray1, raySlope, seg1, seg2, false);
+	}
+	public static float raySegDist(Point ray1, float rayAng, Point[] seg, boolean isAng) {
+		if(isAng) {
+			return raySegDist(ray1, (float) Math.tan(rayAng), seg[0], seg[1]);
+		} else {
+			float raySlope = rayAng;
+			return raySegDist(ray1, raySlope, seg[0], seg[1]);
+		}
+	}
+	
+	public static Point raySegIntersect(Point ray1, float raySlope, Point seg1, Point seg2) {
+		float interX;
+		float interY;
+		float segSlope = slope(seg1, seg2);
+		if(raySlope > 1000000) {
+			// line is vertical
+			interX = ray1.x;
+			interY = segSlope * (ray1.x - seg1.x) + seg1.y;
+		} else {
+			interX = (raySlope * ray1.x - segSlope * seg1.x - ray1.y + seg1.y) / (raySlope - segSlope);
+			interY = raySlope * (interX - ray1.x) + ray1.y;
+		}
+		
+		return new Point(interX, interY);
+	}
+	
+	public static Point raySegIntersect(Point ray1, Point rayThru, Point seg1, Point seg2) {
+		// Returns the intersection point of a ray and line segment given a thru point for the ray
+		float raySlope = slope(ray1, rayThru);
+		return raySegIntersect(ray1, raySlope, seg1, seg2);
+	}
+	public static Point raySegIntersect(Point ray1, float rayAng, Point seg1, Point seg2, boolean isAng) {
+		// Returns the intersection point of a ray and line segment given an angle for the ray
+		if(isAng) {
+			float interX;
+			float interY;
+			float segSlope;
+			float raySlope;
+			if(Math.abs(rayAng - UP) < z || Math.abs(rayAng - DOWN) < z) {
+				// ray slope is vertical
+				segSlope = slope(seg1, seg2);
+				interX = ray1.x;
+				interY = segSlope * (ray1.x - seg1.x) + seg1.y;
+			} else if(seg1.x == seg2.x) {
+				// seg slope is vertical
+				raySlope = (float) Math.tan(rayAng);
+				interX = seg1.x;
+				interY = raySlope * (seg1.x - ray1.x) + ray1.y;
+				
+			} else {
+				return raySegIntersect(ray1, (float) Math.tan(rayAng), seg1, seg2);
+			}
+			
+			return new Point(interX, interY);
+		} else {
+			// just reference slope method
+			float raySlope = rayAng;
+			return raySegIntersect(ray1, raySlope, seg1, seg2);
+		}
+	}
+	public static Point raySegIntersect(Point ray1, float rayAng, Point[] seg, boolean isAng) {
+		if(isAng) {
+			return raySegIntersect(ray1, rayAng, seg[0], seg[1], true);
+		} else {
+			return raySegIntersect(ray1, rayAng, seg[0], seg[1]);
+		}
+	}
+	
+	public static boolean angleFallsIn(float testAng, float min, float max) {
+		// Returns whether or not an angle falls within a minimum and maximum range
+		testAng -= min;
+		testAng = simpAngle(testAng);
+		max -= min;
+		max = simpAngle(max);
+		
+		if(testAng > 0 && testAng < max) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static float simpAngle(float ang) {
+		// Returns ang within a range of 0 and 2pi
+		if(ang < 0) {
+			ang += FULL;
+		} else if(ang > FULL) {
+			ang -= FULL;
+		}
+		return ang;
+	}
+	
+	public static int orientation(Point p, Point q, Point r) {
+		// Returns the orientation of three points (i.e: CW, CWW, collinear)
+	    float val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y); 
+	    
+	    if(val == 0) {
+	    	return 0; //collinear
+	    } else if(val > 0) {
+	    	return 1; //CW
+	    } else {
+	    	return -1; //CCW
+	    }
+	}
+	public static int orientation(Point[] seg, Point r) {
+		return orientation(seg[0], seg[1], r);
+	}
+	
+	public static boolean intersects(Point p1, Point q1, Point p2, Point q2) {
+		// Returns whether or not two line segments intersect
+		int o1 = orientation(p1, q1, p2); 
+	    int o2 = orientation(p1, q1, q2); 
+	    int o3 = orientation(p2, q2, p1); 
+	    int o4 = orientation(p2, q2, q1);
+	    
+	    if (o1 != o2 && o3 != o4) {
+	    	return true;
+	    }
+	    return false;
+	    // (!!!) doesn't cover collinear pairs
+	}
+	
+	public static float angleBoost(float ang1, float ang2) {
+		// Returns the angle of ang1 reflected over ang2
+		
+		/*if(ang2 < ang1) {
+			System.out.println("test");
+			ang2 += 2 * Math.PI;
+		}*/
+		float result = 2 * ang2 - ang1;
+		result = simpAngle(result);
+		return result;
+	}
+	
+	public static Point[] segCircIntersects(Point seg1, Point seg2, Point C, float R) {
+		// Returns the intersection points between a circle at point C radius R, and a line along seg
+		// compute the distance between seg1 and seg2
+		float segDist = distance(seg1, seg2);
+
+		// compute the direction vector D from seg1 to seg2
+		float Dx = (seg2.x - seg1.x) / segDist;
+		float Dy = (seg2.y - seg1.y) / segDist;
+
+		// the equation of the line seg is x = Dx*t + seg1.x, y = Dy*t + seg1.y with 0 <= t <= LAB.
+
+		// compute the distance between the points A and E, where
+		// E is the point on seg closest the circle center (Cx, Cy)
+		float t = Dx * (C.x - seg1.x) + Dy * (C.y - seg1.y);
+
+		// compute the coordinates of the point E
+		Point E = new Point(t * Dx + seg1.x, t * Dy + seg1.y);
+
+		// compute the euclidean distance between E and C
+		float LEC = distance(E, C);
+
+		// test if the line intersects the circle
+		if(LEC < R) {
+			// compute distance from t to circle intersection point
+			float dt = (float) Math.sqrt(R * R - LEC * LEC);
+			
+			// compute first intersection point
+			float Fx = (t - dt) * Dx + seg1.x;
+			float Fy = (t - dt) * Dy + seg1.y;
+			
+			// compute second intersection point
+			float Gx = (t + dt) * Dx + seg1.x;
+			float Gy = (t + dt) * Dy + seg1.y;
+			
+			Point[] points = new Point[2];
+			points[0] = new Point(Fx, Fy);
+			points[1] = new Point(Gx, Gy);
+			return points;
+			
+		} else {
+			// line doesn't touch circle or is tangent (treat tangent as no touch)
+			return null;
+		}
+	}
+	public static Point[] segCircIntersects(Point[] seg, Point C, float R) {
+		return segCircIntersects(seg[0], seg[1], C, R);
+	}
+	
+	public static boolean firstIsMin(float ang1, float ang2) {
+		// Returns the most CCW angle assuming the angle between both rays is less than 180 degrees
+		return (Math.abs(ang1 - ang2) > PI ^ ang1 - ang2 < 0);
+	}
+	
+	public static boolean pointFallsWithin(Point pCheck, Point p1, Point p2) {
+		// Returns whether or not pCheck falls within the rectangle formed by p1 and p2
+		if(p1.x - pCheck.x > 0 ^ p2.x - pCheck.x > 0) {
+			// falls in x boundary
+			if(p1.y - pCheck.y > 0 ^ p2.y - pCheck.y > 0) {
+				// falls in y boundary
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean rectIntersect(Point p1, Point p2, Point q1, Point q2) {
+		// Returns whether or not the rectangle formed by corner points p1 and p2
+		//     intersects the rectangle formed by corner points q1 and q2
+		float pMin = Math.min(p1.x, p2.x);
+		float pMax = Math.max(p1.x, p2.x);
+		float qMin = Math.min(q1.x, q2.x);
+		float qMax = Math.max(q1.x, q2.x);
+		if(pMin > qMax || qMin > pMax) {
+			return false; 
+		}
+		
+		if(!(Math.abs(p1.y - q1.y) < z && Math.abs(p2.y - q2.y) < z && Math.abs(p1.y - p2.y) < z)) {
+			// same y protection
+			pMin = Math.min(p1.y, p2.y);
+			pMax = Math.max(p1.y, p2.y);
+			qMin = Math.min(q1.y, q2.y);
+			qMax = Math.max(q1.y, q2.y);
+			if(pMin > qMax || qMin > pMax) {
+				return false; 
+			}
+		}
+		
+		return true;
+	}
+	public static boolean rectIntersect(Point[] rect1, Point[] rect2) {
+		return rectIntersect(rect1[0], rect1[1], rect2[0], rect2[1]);
+	}
+	
+	public static Point center(Point p1, Point p2) {
+		// Returns a new point in the middle of p1 and p2
+		return new Point((p1.x + p2.x) / 2, (p1.y + p2.y) / 2);
+	}
+
+	public static boolean aboutEqual(float f1, float f2) {
+		if(Math.abs(f1 - f2) < z) {
+			return true;
+		}
+		return false;
+	}
+	
+	/*
+	public static void main(String[] args) {
+		Point a = new Point(1, 2);
+		Point b = new Point(4, 5);
+		Point c = new Point(3, 4);
+		
+		System.out.println(c.fallsWithin(a, b));
+	}
+	*/
+	
+}
